ca17597bd8047e362a68a86b5fd66288179c2b28
unknown <you@example.com>
2021-04-15 16:38:16 -0500

    some note

---
+++ HeroesOfPymoli/main.py
@@ -0,0 +1,9 @@
+#Dependencies and Setup
+import pandas as pd 
+
+#File to Load 
+purchase_data_df = 'Resources/purchase_data.csv'
+
+# Read purchasing File and store into Pandas data frame 
+purchase_data = pd.read_csv(purchase_data_df)
+
